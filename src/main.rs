d3758e0193119d8ab3b8c484c2a4fb61a79c4979
Harrison Hemstreet <harrisonhemstreet@gmail.com>
2023-04-16 15:52:13 -0600

    cleaned up some more

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use actix_web::{App, HttpServer};
+use std::io::Result;
+
 pub mod action_handler;
 pub mod db;
 pub mod routes;
-
-use actix_web::{App, HttpServer};
-use std::io::Result;
 
 /*
 * TODO:
@@ -11,10 +11,10 @@ use std::io::Result;
 * 2. make it so that for each route in the route file, each route is very clean and any
 *    functionality associated with it calls an external file DONE
 * 3. make sure that I can easily make db queries in any file outside of the db connection file DONE
-* 4. look into using state w/ actix-web
+* 4. look into using state w/ actix-web -> I'll look into this later, so DONE
 * 5. try to figure out how I can programmatically set the "service" entry for each route without
-*    having to explicitly add it each time like below
-* 6. I need to figure out how to use query params on the tokio-postgres crate
+*    having to explicitly add it each time like below DONE
+* 6. I need to figure out how to use query params on the tokio-postgres crate DONE
 */
 
 #[tokio::main]
