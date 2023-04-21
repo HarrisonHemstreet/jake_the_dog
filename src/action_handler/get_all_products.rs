3ab12174d713ebbe29fee0dbcc00f630f4718b84
Harrison Hemstreet <harrisonhemstreet@gmail.com>
2023-04-20 21:57:48 -0600

    fixed my errors with my new route

--- src/action_handler/get_all_products.rs
+++ src/action_handler/get_all_products.rs
@@ -1,45 +1,48 @@
 use serde::{Serialize, Deserialize};
-use tokio_postgres::types::{FromSql, ToSql, Type};
-// use tokio_postgres::error::Error;
-use serde::ser::StdError;
 use serde_json;
 use crate::db::query;
 use crate::db::QueryBuilder;
+use postgres_types::{FromSql, ToSql};
 
-#[derive(Debug, PartialEq, FromSql, ToSql)]
+#[derive(Debug, PartialEq, FromSql, ToSql, Serialize, Deserialize)]
+#[postgres(name = "variant_type")]
 enum VariantType {
+    #[postgres(name = "color")]
     Color,
+    #[postgres(name = "image")]
     Image
 }
 
-// enum status {
-//     "New in",
-//     "Limited Edition",
-//     "Sold out",
-//     "50% Discount"
-// }
-
-// /*
-impl<'a> FromSql<'a> for VariantType {
-    fn from_sql(ty: &Type, raw: &'a [u8]) -> Result<VariantType, Box<(dyn StdError + Send + Sync + 'static)>> {
-        println!("IN FROM SQL!");
-        println!("raw: {:?}", raw);
-        match raw {
-            b"color" => Ok(VariantType::Color),
-            b"image" => Ok(VariantType::Image),
-            _ => Err(Box::new(std::fmt::Error)),
-        }
-    }
-
-    fn accepts(ty: &Type) -> bool {
-        // This implementation returns true if the type is a string type
-        match ty.name() {
-            "varchar" | "text" => true,
-            _ => false,
-        }
-    }
+#[derive(Debug, PartialEq, FromSql, ToSql, Serialize, Deserialize)]
+#[postgres(name = "status")]
+enum Status {
+    #[postgres(name = "New in")]
+    NewIn,
+    #[postgres(name = "Sold Out")]
+    SoldOut,
+    #[postgres(name = "50% Discount")]
+    HalfOff,
+    #[postgres(name = "Limited Edition")]
+    LimitedEdition
 }
-// */
+
+/*
+fn process_status(mut status: &str) -> &str {
+    if status == "NewIn" {
+        status = "New In";
+    }
+    if status == "SoldOut" {
+        status = "Sold Out";
+    }
+    if status == "HalfOff" {
+        status = "50% Discount";
+    }
+    if status == "LimitedEdition" {
+        status = "Limited Edition";
+    }
+    status
+}
+*/
 
 pub async fn execute() -> String {
     #[derive(Serialize, Deserialize, Debug)]
@@ -53,10 +56,10 @@ pub async fn execute() -> String {
         ar_tags: Vec<&'a str>,
         ds_link: &'a str,
         ar_variants: Vec<&'a str>,
-        // en_variant_type: VariantType,
+        en_variant_type: VariantType,
         ar_sizes: Vec<&'a str>,
         ar_all_of_sizes: Vec<&'a str>,
-        // en_status: &'a str,
+        en_status: Status,
         ds_rating: &'a str,
         vi_number_of_reviews: i32
     }
@@ -73,10 +76,10 @@ pub async fn execute() -> String {
         ar_tags: rows[0].get(6),
         ds_link: rows[0].get(7),
         ar_variants: rows[0].get(8),
-        // en_variant_type: rows[0].get(9),
+        en_variant_type: rows[0].get(9),
         ar_sizes: rows[0].get(10),
         ar_all_of_sizes: rows[0].get(11),
-        // en_status: rows[0].get(12),
+        en_status: rows[0].get(12),
         ds_rating: rows[0].get(13),
         vi_number_of_reviews: rows[0].get(14)
     };
