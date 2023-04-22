e550f97ba755f035c4bda6a9768dd8c059acbce2
Harrison Hemstreet <harrisonhemstreet@gmail.com>
2023-04-21 22:16:50 -0600

    made some notes/todos

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -11,10 +11,36 @@ async fn db_test() -> impl Responder {
     action_handler::db_test::execute().await
 }
 
-#[get("/get_all_products")]
+#[get("/products")]
 async fn get_all_products() -> impl Responder {
     action_handler::get_all_products::execute().await
 }
+
+/*
+TODO:
+#[get("/product")]
+async fn get_all_products() -> impl Responder {
+    // should take in an id and return that specific product
+    action_handler::get_all_products::execute().await
+}
+
+#[post("/product")]
+async fn get_all_products() -> impl Responder {
+    // should create a product
+    action_handler::get_all_products::execute().await
+}
+#[put("/product")]
+async fn get_all_products() -> impl Responder {
+    // should update a product
+    action_handler::get_all_products::execute().await
+}
+#[delete("/product")]
+async fn get_all_products() -> impl Responder {
+    // should delete a product
+    action_handler::get_all_products::execute().await
+}
+*/
+
 
 pub fn routes() -> impl HttpServiceFactory {
     (
@@ -23,3 +49,7 @@ pub fn routes() -> impl HttpServiceFactory {
         get_all_products
     )
 }
+/*
+* 1. create some basic post, update, delete requests on product now. I want to make sure I am
+*    making ACID apis (i think that's the term?)
+*/
